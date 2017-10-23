e8cf74bbdfd1efa8bfa7216f5d0ac8ce5972b555
Koustuv Sinha <koustuv.sinha@mail.mcgill.ca>
2017-10-23 18:31:43 -0400

    Add new experiment for debiasing

--- word_vectors/generate.py
+++ word_vectors/generate.py
@@ -13,6 +13,7 @@ import torch.nn.functional as F
 import numpy as np
 import pandas as pd
 import copy
+from tqdm import tqdm
 
 import data
 
@@ -41,6 +42,10 @@ parser.add_argument('--start_samples', type=str,
                     help='csv file containing the start tokens')
 parser.add_argument('--header_column', type=str,
                     help='csv file header')
+parser.add_argument('--stochastic', action='store_true',
+                    help='Run the stochastic experiment')
+parser.add_argument('--stoch_times', type=int, default=1000,
+                    help='Run the stochastic experiment n times')
 args = parser.parse_args()
 
 if args.temperature < 1e-3:
@@ -54,9 +59,14 @@ with open(args.checkpoint, 'rb') as f:
 corpus = data.Corpus(args.data, load=True, prefix='google')
 ntokens = len(corpus.dictionary)
 
+# gender centric words
+male_words = ["he", "him", "his", "himself", "husband", "man", "men", "boy"]
+female_words = ["she", "her", "herself", "wife", "woman", "women", "girl"]
 
-def generate_sentence(starting_token):
-    # Set the random seed manually for reproducibility.
+
+def set_seed():
+    """ Set the random seed manually for reproducibility.
+    """
     torch.manual_seed(args.seed)
     if torch.cuda.is_available():
         if not args.cuda:
@@ -64,6 +74,11 @@ def generate_sentence(starting_token):
                 "WARNING: You have a CUDA device, so you should probably run with --cuda")
         else:
             torch.cuda.manual_seed(args.seed)
+
+
+def generate_sentence(starting_token, fix_seed=True):
+    if fix_seed:
+        set_seed()
     cmodel = copy.deepcopy(model)
     if args.cuda:
         cmodel.cuda()
@@ -96,6 +111,9 @@ def generate_sentence(starting_token):
         input.data.fill_(word_idx)
         # print input
         word = corpus.dictionary.idx2word[word_idx]
+        # break on end of sentence
+        if word == '<eos>':
+            break
         response.append(word)
     response = ' '.join(response)
     return response
@@ -105,18 +123,59 @@ samples = None
 
 if args.start_samples:
     samples = pd.read_csv(args.start_samples)
-    samples[args.header_column] = ''
-
-    if len(samples) > 0:
+    if args.stochastic:
+        samples['male_percentage'] = 0.0
+        samples['female_percentage'] = 0.0
+        samples['unbiased_percentage'] = 0.0
+        pb = tqdm(total=len(samples))
+        # run the stochastic experiment
         for i, row in samples.iterrows():
-            token = row['token']
-            try:
-                response = generate_sentence(token)
-                samples.set_value(i, args.header_column, response)
-            except Exception as e:
-                print e
-            print 'Generated {} / {} sentence'.format(i, len(samples))
+            # set the seed first and then dont
+            set_seed()
+            male_count = 0
+            female_count = 0
+            pbs = tqdm(total=args.stoch_times)
+            for step in range(args.stoch_times):
+                response = generate_sentence(
+                    row['token'], fix_seed=False).split(' ')
+                for word in male_words:
+                    if word in response:
+                        male_count += 1
+                        break
+                for word in female_words:
+                    if word in response:
+                        female_count += 1
+                        break
+                pbs.update(1)
+            pbs.close()
+            if male_count + female_count > args.stoch_times:
+                extra = male_count + female_count - args.stoch_times
+                male_count = male_count - extra
+                female_count = female_count - extra
+            unbiased = args.stoch_times - (male_count + female_count)
+            male_perc = (1.0 * male_count) / args.stoch_times
+            female_perc = (1.0 * female_count) / args.stoch_times
+            unbiased_perc = (1.0 * unbiased) / args.stoch_times
+            samples.set_value(i, 'male_percentage', male_perc)
+            samples.set_value(i, 'female_percentage', female_perc)
+            samples.set_value(i, 'unbiased_percentage', unbiased_perc)
+            pb.update(1)
+        pb.close()
         samples.to_csv(args.start_samples, encoding='utf-8', index=None)
+    else:
+        # run the sample generation experiment
+        samples[args.header_column] = ''
+
+        if len(samples) > 0:
+            for i, row in samples.iterrows():
+                token = row['token']
+                try:
+                    response = generate_sentence(token)
+                    samples.set_value(i, args.header_column, response)
+                except Exception as e:
+                    print e
+                print 'Generated {} / {} sentence'.format(i, len(samples))
+            samples.to_csv(args.start_samples, encoding='utf-8', index=None)
 else:
     token = args.token
     response = generate_sentence(token)
